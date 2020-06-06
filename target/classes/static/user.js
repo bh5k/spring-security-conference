6d74d89ab1c246b058cd8e3a562b3c8c22257507
Bryan <bryankhansen@gmail.com>
2020-06-06 14:24:10 -0600

    Revert "Initial commit"
    This reverts commit a3652b015ff5d8a2bb51f16294b2be13429f2f7a.

--- target/classes/static/user.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/conference/user"
-    }).then(function(data) {
-        $('.firstname').append(data.firstname);
-        $('.lastname').append(data.lastname);
-        $('.age').append(data.age);
-    });
-});
