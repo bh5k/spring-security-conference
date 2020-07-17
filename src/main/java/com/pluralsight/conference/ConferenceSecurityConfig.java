2d4022a20586f33dc7b270af2afb6de57ab2c59b
Bryan <bryankhansen@gmail.com>
2020-07-17 02:42:25 -0600

    Working example using h2

--- src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -1,18 +1,24 @@
 package com.pluralsight.conference;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -34,8 +40,14 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("bryan").password(passwordEncoder().encode("pass")).roles("USER");
+        //auth.inMemoryAuthentication()
+        //        .withUser("bryan").password(passwordEncoder().encode("pass")).roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("bryan")
+                        .password(passwordEncoder().encode("pass"))
+                        .roles("USER"));
 
     }
 
