dc3a3123918383ae9306611bba56f8f3683350fb
Bryan <bryankhansen@gmail.com>
2020-08-02 20:19:51 -0600

    Trial working for recording.

--- src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pluralsight.conference;
 
+import com.pluralsight.conference.service.ConferenceUserDetailsContextMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,6 +19,9 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
+    @Autowired
+    private ConferenceUserDetailsContextMapper conferenceUserDetailsContextMapper;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -53,7 +56,9 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .passwordCompare()
                 .passwordEncoder(passwordEncoder())
-                .passwordAttribute("userPassword");
+                .passwordAttribute("userPassword")
+                .and()
+                .userDetailsContextMapper(conferenceUserDetailsContextMapper);
 
     }
 
---
+++ src/main/java/com/pluralsight/conference/model/ConferenceUserDetails.java
@@ -0,0 +1,23 @@
+package com.pluralsight.conference.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class ConferenceUserDetails extends org.springframework.security.core.userdetails.User {
+
+    private String nickname;
+
+    public ConferenceUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
